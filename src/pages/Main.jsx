41bdf7f38a710e8f9867a3b41f5d058d935ee4f4
Lautaro770 <posadalautaro@gmail.com>
2025-10-15 23:48:23 -0300

    feat: complete WhatsApp clone with all dependencies

---
+++ src/pages/Main.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
+import ChatList from '../components/ChatList';
+
+const Main = () => {
+  return (
+    <div className="sidebar">
+      <Header />
+      <SearchBar />
+      <ChatList />
+    </div>
+  );
+};
+
+export default Main;

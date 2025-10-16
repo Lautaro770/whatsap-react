b68c8159ca4f0872a2134c794c32a4174662a84a
Lautaro770 <posadalautaro@gmail.com>
2025-10-15 22:59:56 -0300

    fix: correct Home.jsx syntax and remove corrupted content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,16 @@
-﻿C:\Users\Lautaro
+import React from 'react';
+import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
+import ChatList from '../components/ChatList';
+
+const Home = () => {
+  return (
+    <div className="sidebar">
+      <Header />
+      <SearchBar />
+      <ChatList />
+    </div>
+  );
+};
+
+export default Home;
